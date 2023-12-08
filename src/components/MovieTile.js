4264e35cd54997cb3988bc523e4147c42217d669
sanjeev <v-sanjgarg@expediagroup.com>
2023-12-08 21:14:04 +0530

    fix

--- src/components/MovieTile.js
+++ src/components/MovieTile.js
@@ -1,22 +1,68 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const MovieTile = ({ movieInfo , onDelete: handlDelete , handleClick, handleEditClick}) => {
+import React, { useState, useRef, useEffect } from 'react';
+import ContextMenu from './ContextMenu';
+import './MovieTile.css';
+
+const MovieTile = ({ movieInfo, handleClick, handleEditClick, handleDeleteClick }) => {
   const { id, title, poster_path } = movieInfo;
   const imgStyle = {
-    maxWidth: '150px', // Set the maximum width
-    maxHeight: '200px', // Set the maximum height
+    maxWidth: '150px',
+    maxHeight: '200px',
   };
 
+  const [contextMenuPosition, setContextMenuPosition] = useState({ x: 0, y: 0 });
+
+  const handleContextMenu = (e) => {
+    e.preventDefault();
+    setContextMenuPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const closeContextMenu = () => {
+    setContextMenuPosition({ x: 0, y: 0 });
+  };
+
+  const contextMenuRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (contextMenuRef.current && !contextMenuRef.current.contains(event.target)) {
+        closeContextMenu();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
   return (
-    <div className="movie-tile" data-testid={`movie-tile-${id}`}>
+    <div className="movie-tile" data-testid={`movie-tile-${id}`} onContextMenu={handleContextMenu}>
       <div onClick={() => handleClick(movieInfo)}>
         <img src={poster_path} alt={title} style={imgStyle} />
-        <h2>{title}</h2>  
-      </div>  
-      <button onClick={() => handlDelete(id)}>delete</button>
-      <button onClick={() => handleEditClick(movieInfo)} className="edit-movie-button">
-          Edit Movie
-      </button>
+        <h2>{title}</h2>
+      </div>
+      {contextMenuPosition.x !== 0 && (
+        <div
+          ref={contextMenuRef}
+          style={{
+            position: 'fixed',
+            top: contextMenuPosition.y,
+            right: window.innerWidth - contextMenuPosition.x,
+          }}
+        >
+          <ContextMenu
+            onDelete={() => {
+              handleDeleteClick(id);
+              closeContextMenu();
+            }}
+            onEdit={() => {
+              handleEditClick(movieInfo);
+              closeContextMenu();
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 };
